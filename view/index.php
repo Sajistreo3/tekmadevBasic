8d464d89948d1bbbcb8abaa36861879d850d2e7e
Shajeed <shajeed@live.fr>
2022-08-16 12:47:41 -0400

    updated price and portfolio

--- view/index.php
+++ view/index.php
@@ -1,83 +1,83 @@
 <?php
-  include "header.php";
+include "header.php";
 ?>
 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
-    <div class="container position-relative" data-aos="fade-up" data-aos-delay="500">
-      <h1>Welcome to Tekmadev</h1>
-      <h2>We are team of talented designers making websites</h2>
-      <a href="#about" class="btn-get-started scrollto">Get Started</a>
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+  <div class="container position-relative" data-aos="fade-up" data-aos-delay="500">
+    <h1>Welcome to Tekmadev</h1>
+    <h2>We are team of talented designers making websites</h2>
+    <a href="#about" class="btn-get-started scrollto">Get Started</a>
+  </div>
+</section><!-- End Hero -->
+
+<main id="main">
+
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left">
+          <img src="assets/img/about.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
+          <h3>At Tekmadev, we take things closer than you think.</h3>
+          <p class="fst-italic">
+            We are a young Team aiming to make your ideas access to everyone at the best price.
+          </p>
+          <ul>
+            <li><i class="bi bi-check-circle"></i> Listen and prepare for what matters for you and your business.</li>
+            <li><i class="bi bi-check-circle"></i> Plan and make the best strategy by focusing on the issues calculate important risks</li>
+            <li><i class="bi bi-check-circle"></i> We will Design what is pleasing to the User to bring growth to your business by minimizing the risks and waste. Making your idea meaning full to the user interaction and experience.</li>
+          </ul>
+          <p>
+            <!-- other text -->
+          </p>
+        </div>
+      </div>
+
     </div>
-  </section><!-- End Hero -->
+  </section>
+  <!-- End About Section -->
 
-  <main id="main">
+  <!-- ======= Why Us Section ======= -->
+  <section id="why-us" class="why-us">
+    <div class="container">
 
-    <!-- ======= About Section ======= -->
-    <section id="about" class="about">
-      <div class="container">
+      <div class="row">
 
-        <div class="row">
-          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left">
-            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+        <div class="col-lg-4" data-aos="fade-up">
+          <div class="box">
+            <span>01</span>
+            <h4>We are here to Listen</h4>
+            <p>Listen and prepare for what matters for you and your business.</p>
           </div>
-          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
-            <h3>At Tekmadev, we take things closer than you think.</h3>
-            <p class="fst-italic">
-              We are a young Team aiming to make your ideas access to everyone at the best price.
-            </p>
-            <ul>
-              <li><i class="bi bi-check-circle"></i> Listen and prepare for what matters for you and your business.</li>
-              <li><i class="bi bi-check-circle"></i> Plan and make the best strategy by focusing on the issues calculate important risks</li>
-              <li><i class="bi bi-check-circle"></i> We will Design what is pleasing to the User to bring growth to your business by minimizing the risks and waste. Making your idea meaning full to the user interaction and experience.</li>
-            </ul>
-            <p>
-              <!-- other text -->
-            </p>
+        </div>
+
+        <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="150">
+          <div class="box">
+            <span>02</span>
+            <h4>Calculated Work</h4>
+            <p>Plan and make the best strategy by focusing on the issues and calculate important risks</p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="300">
+          <div class="box">
+            <span>03</span>
+            <h4>The Perfect Solution</h4>
+            <p>We will Design what is pleasing to the User to bring growth to your business by minimizing the risks and waste. Making your idea meaning full to the user interaction and experience.</p>
           </div>
         </div>
 
       </div>
-    </section>
-    <!-- End About Section -->
 
-    <!-- ======= Why Us Section ======= -->
-    <section id="why-us" class="why-us">
-      <div class="container">
+    </div>
+  </section><!-- End Why Us Section -->
 
-        <div class="row">
-
-          <div class="col-lg-4" data-aos="fade-up">
-            <div class="box">
-              <span>01</span>
-              <h4>We are here to Listen</h4>
-              <p>Listen and prepare for what matters for you and your business.</p>
-            </div>
-          </div>
-
-          <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="150">
-            <div class="box">
-              <span>02</span>
-              <h4>Calculated Work</h4>
-              <p>Plan and make the best strategy by focusing on the issues and calculate important risks</p>
-            </div>
-          </div>
-
-          <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="300">
-            <div class="box">
-              <span>03</span>
-              <h4>The Perfect Solution</h4>
-              <p>We will Design what is pleasing to the User to bring growth to your business by minimizing the risks and waste. Making your idea meaning full to the user interaction and experience.</p>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Why Us Section -->
-
-    <!-- ======= Clients Section ======= -->
-    <!-- <section id="clients" class="clients">
+  <!-- ======= Clients Section ======= -->
+  <!-- <section id="clients" class="clients">
       <div class="container" data-aos="zoom-in">
 
         <div class="row d-flex align-items-center">
@@ -110,46 +110,46 @@
 
       </div>
     </section> -->
-    <!-- End Clients Section -->
-
-
-    
+  <!-- End Clients Section -->
 
 
 
-    <?php 
-      include "service.php";
 
-      include "cta.php";
 
-      include "portfolio.php";
 
-      include "pricing.php";
+  <?php
+  include "service.php";
 
-      include "team.php";
+  include "cta.php";
 
-      include "contact.php";
-    ?>
+  // include "portfolio.php";
 
-    
+  // include "pricing.php";
 
-  </main><!-- End #main -->
+  include "team.php";
 
-  <?php include "footer.php"; ?>
+  include "contact.php";
+  ?>
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-  <div id="preloader"></div>
 
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
 
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+</main><!-- End #main -->
+
+<?php include "footer.php"; ?>
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
 
 </body>
 
